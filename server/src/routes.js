5bc4f2f9bff53673720c7c570d59f5b071071631
Kristian <krdo@eaaa.dk>
2020-10-01 11:00:38 +0200

    Removing unused parameters.

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,10 +1,14 @@
-module.exports = (db, contentDir) => {
+module.exports = () => {
   const express = require("express");
   const router = express.Router();
 
   /**** Routes ****/
-   router.get('/hello', async (req, res) => {
+  router.get('/hello', async (req, res) => {
     res.json({msg: "Hello, world!"});
+  });
+
+  router.get('/hello/:name', async (req, res) => {
+    res.json({msg: `Hello, ${req.params.name}`});
   });
 
   return router;
